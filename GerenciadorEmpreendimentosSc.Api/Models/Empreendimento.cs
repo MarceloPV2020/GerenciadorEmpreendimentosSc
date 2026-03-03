c847cf47d73c01b08cc9da2ef7eb88a54eb16d9d
MarceloPV2020 <marcelo.parisotto2@gmail.com>
2026-03-03 20:02:19 -0300

    Models

---
+++ GerenciadorEmpreendimentosSc.Api/Models/Empreendimento.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GerenciadorEmpreendimentosSc.Api.Models;
+
+public sealed class Empreendimento
+{
+    [Key]
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    [Required, MinLength(2), MaxLength(120)]
+    public string NomeEmpreendimento { get; set; } = default!;
+
+    [Required, MinLength(2), MaxLength(120)]
+    public string NomeEmpreendedor { get; set; } = default!;
+
+    [Required, MinLength(2), MaxLength(80)]
+    public string Municipio { get; set; } = default!;
+
+    [Required]
+    public Segmento Segmento { get; set; }
+
+    // Somente telefone
+    [Required, Phone, MinLength(8), MaxLength(20)]
+    public string Telefone { get; set; } = default!;
+
+    [Required]
+    public StatusEmpreendimento Status { get; set; } = StatusEmpreendimento.Ativo;
+
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+    public DateTimeOffset UpdatedAt { get; set; } = DateTimeOffset.UtcNow;
+}
