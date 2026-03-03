2af7e74d0beeaf0d9a21b54550aac7d62cb0d11d
MarceloPV2020 <marcelo.parisotto2@gmail.com>
2026-03-03 19:58:46 -0300

    Models

---
+++ GerenciadorEmpreendimentosSc.Api/Models/Segmento.cs
@@ -0,0 +1,11 @@
+﻿namespace GerenciadorEmpreendimentosSc.Api.Models
+{
+    public enum Segmento
+    {
+        Tecnologia = 1,
+        Comercio = 2,
+        Industria = 3,
+        Servicos = 4,
+        Agronegocio = 5
+    }
+}
---
+++ GerenciadorEmpreendimentosSc.Api/Models/StatusEmpreendimento.cs
@@ -0,0 +1,8 @@
+﻿namespace GerenciadorEmpreendimentosSc.Api.Models
+{
+    public enum StatusEmpreendimento
+    {
+        Ativo = 1,
+        Inativo = 2
+    }
+}
