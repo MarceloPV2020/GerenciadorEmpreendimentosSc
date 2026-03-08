b093bfc717a36df068e40a81938395b3a1a552bb
MarceloPV2020 <marcelo.parisotto2@gmail.com>
2026-03-08 13:04:40 -0300

    Models

--- GerenciadorEmpreendimentosSc.Api/Program.cs
+++ GerenciadorEmpreendimentosSc.Api/Program.cs
@@ -1,10 +1,35 @@
+<<<<<<< Updated upstream
+=======
+using FluentValidation;
+using GerenciadorEmpreendimentosSc.Api.Data;
+using GerenciadorEmpreendimentosSc.Api.Dtos;
+using Microsoft.EntityFrameworkCore;
+
+>>>>>>> Stashed changes
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+<<<<<<< Updated upstream
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+=======
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddProblemDetails();
+builder.Services.AddHealthChecks();
+
+builder.Services.AddValidatorsFromAssemblyContaining<EmpreendimentoCreateDto.Validator>();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    var cs = builder.Configuration.GetConnectionString("Default");
+    opt.UseSqlite(cs);
+});
+>>>>>>> Stashed changes
 
 var app = builder.Build();
 
